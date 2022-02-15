b8afb931cd1b2535c6acc94cea1485d29b63e3ce
Bora TÜZÜN <boratzn94@gmail.com>
2022-02-15 13:26:22 +0300

    Add files via upload

---
+++ SafakSayar/SafakSayar/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace SafakSayar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblEgitimBitis = new System.Windows.Forms.Label();
+            this.lblEgitimBitis2 = new System.Windows.Forms.Label();
+            this.lblHaftaBitis = new System.Windows.Forms.Label();
+            this.lblHaftaBitis2 = new System.Windows.Forms.Label();
+            this.lblGunBitis = new System.Windows.Forms.Label();
+            this.lblGunBitis2 = new System.Windows.Forms.Label();
+            this.lblMolaBitis = new System.Windows.Forms.Label();
+            this.lblMolaBitis2 = new System.Windows.Forms.Label();
+            this.timerMola = new System.Windows.Forms.Timer(this.components);
+            this.timerGün = new System.Windows.Forms.Timer(this.components);
+            this.timerHafta = new System.Windows.Forms.Timer(this.components);
+            this.timerEgitim = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(270, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şafak Sayar\'ı Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblEgitimBitis
+            // 
+            this.lblEgitimBitis.AutoSize = true;
+            this.lblEgitimBitis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEgitimBitis.Location = new System.Drawing.Point(13, 116);
+            this.lblEgitimBitis.Name = "lblEgitimBitis";
+            this.lblEgitimBitis.Size = new System.Drawing.Size(268, 20);
+            this.lblEgitimBitis.TabIndex = 1;
+            this.lblEgitimBitis.Text = "Eğitimin Bitimine Kalan Süre : ";
+            // 
+            // lblEgitimBitis2
+            // 
+            this.lblEgitimBitis2.AutoSize = true;
+            this.lblEgitimBitis2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEgitimBitis2.ForeColor = System.Drawing.Color.Red;
+            this.lblEgitimBitis2.Location = new System.Drawing.Point(356, 116);
+            this.lblEgitimBitis2.Name = "lblEgitimBitis2";
+            this.lblEgitimBitis2.Size = new System.Drawing.Size(0, 20);
+            this.lblEgitimBitis2.TabIndex = 2;
+            // 
+            // lblHaftaBitis
+            // 
+            this.lblHaftaBitis.AutoSize = true;
+            this.lblHaftaBitis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHaftaBitis.Location = new System.Drawing.Point(13, 178);
+            this.lblHaftaBitis.Name = "lblHaftaBitis";
+            this.lblHaftaBitis.Size = new System.Drawing.Size(271, 20);
+            this.lblHaftaBitis.TabIndex = 1;
+            this.lblHaftaBitis.Text = "Haftanın Bitimine Kalan Süre : ";
+            // 
+            // lblHaftaBitis2
+            // 
+            this.lblHaftaBitis2.AutoSize = true;
+            this.lblHaftaBitis2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHaftaBitis2.ForeColor = System.Drawing.Color.Red;
+            this.lblHaftaBitis2.Location = new System.Drawing.Point(356, 178);
+            this.lblHaftaBitis2.Name = "lblHaftaBitis2";
+            this.lblHaftaBitis2.Size = new System.Drawing.Size(0, 20);
+            this.lblHaftaBitis2.TabIndex = 2;
+            // 
+            // lblGunBitis
+            // 
+            this.lblGunBitis.AutoSize = true;
+            this.lblGunBitis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGunBitis.Location = new System.Drawing.Point(12, 240);
+            this.lblGunBitis.Name = "lblGunBitis";
+            this.lblGunBitis.Size = new System.Drawing.Size(254, 20);
+            this.lblGunBitis.TabIndex = 1;
+            this.lblGunBitis.Text = "Günün Bitimine Kalan Süre : ";
+            // 
+            // lblGunBitis2
+            // 
+            this.lblGunBitis2.AutoSize = true;
+            this.lblGunBitis2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGunBitis2.ForeColor = System.Drawing.Color.Red;
+            this.lblGunBitis2.Location = new System.Drawing.Point(355, 240);
+            this.lblGunBitis2.Name = "lblGunBitis2";
+            this.lblGunBitis2.Size = new System.Drawing.Size(0, 20);
+            this.lblGunBitis2.TabIndex = 2;
+            // 
+            // lblMolaBitis
+            // 
+            this.lblMolaBitis.AutoSize = true;
+            this.lblMolaBitis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMolaBitis.Location = new System.Drawing.Point(12, 298);
+            this.lblMolaBitis.Name = "lblMolaBitis";
+            this.lblMolaBitis.Size = new System.Drawing.Size(184, 20);
+            this.lblMolaBitis.TabIndex = 1;
+            this.lblMolaBitis.Text = "Molaya Kalan Süre : ";
+            // 
+            // lblMolaBitis2
+            // 
+            this.lblMolaBitis2.AutoSize = true;
+            this.lblMolaBitis2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMolaBitis2.ForeColor = System.Drawing.Color.Red;
+            this.lblMolaBitis2.Location = new System.Drawing.Point(355, 298);
+            this.lblMolaBitis2.Name = "lblMolaBitis2";
+            this.lblMolaBitis2.Size = new System.Drawing.Size(0, 20);
+            this.lblMolaBitis2.TabIndex = 2;
+            // 
+            // timerMola
+            // 
+            this.timerMola.Interval = 1000;
+            this.timerMola.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerGün
+            // 
+            this.timerGün.Interval = 1000;
+            this.timerGün.Tick += new System.EventHandler(this.timerGün_Tick);
+            // 
+            // timerHafta
+            // 
+            this.timerHafta.Interval = 1000;
+            this.timerHafta.Tick += new System.EventHandler(this.timerHafta_Tick);
+            // 
+            // timerEgitim
+            // 
+            this.timerEgitim.Interval = 1000;
+            this.timerEgitim.Tick += new System.EventHandler(this.timerEgitim_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 343);
+            this.Controls.Add(this.lblMolaBitis2);
+            this.Controls.Add(this.lblGunBitis2);
+            this.Controls.Add(this.lblHaftaBitis2);
+            this.Controls.Add(this.lblEgitimBitis2);
+            this.Controls.Add(this.lblMolaBitis);
+            this.Controls.Add(this.lblGunBitis);
+            this.Controls.Add(this.lblHaftaBitis);
+            this.Controls.Add(this.lblEgitimBitis);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblEgitimBitis;
+        private System.Windows.Forms.Label lblEgitimBitis2;
+        private System.Windows.Forms.Label lblHaftaBitis;
+        private System.Windows.Forms.Label lblHaftaBitis2;
+        private System.Windows.Forms.Label lblGunBitis;
+        private System.Windows.Forms.Label lblGunBitis2;
+        private System.Windows.Forms.Label lblMolaBitis;
+        private System.Windows.Forms.Label lblMolaBitis2;
+        private System.Windows.Forms.Timer timerMola;
+        private System.Windows.Forms.Timer timerGün;
+        private System.Windows.Forms.Timer timerHafta;
+        private System.Windows.Forms.Timer timerEgitim;
+    }
+}
+
---
+++ SafakSayar/SafakSayar/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SafakSayar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitSettings();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timerMola.Start();
+            timerGün.Start();
+            timerHafta.Start();
+            timerEgitim.Start();
+        }
+
+        public void InitSettings()
+        {
+            lblEgitimBitis.Visible = false;
+            lblEgitimBitis2.Visible = false;
+            lblGunBitis.Visible = false;    
+            lblGunBitis2.Visible = false;
+            lblHaftaBitis.Visible = false;
+            lblHaftaBitis2.Visible = false;
+            lblMolaBitis.Visible = false;
+            lblMolaBitis2.Visible = false;
+        }
+
+        public void LabelsControl()
+        {
+            lblEgitimBitis.Visible = true;
+            lblEgitimBitis2.Visible = true;
+            lblGunBitis.Visible = true;
+            lblGunBitis2.Visible = true;
+            lblHaftaBitis.Visible = true;
+            lblHaftaBitis2.Visible = true;
+            lblMolaBitis.Visible = true;
+            lblMolaBitis2.Visible = true;
+        }
+        string[] molalar = new[] { "16:30", "18:30", "20:00"};
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string girisZamani = DateTime.Now.ToLongTimeString();
+            string cikisZamani = "";
+            string calismaSuresi = "";
+            TimeSpan girisCikisFarki = new TimeSpan(0, 0, 0, 0,0);
+            int index = 0;
+
+            for (int i = 0; i < molalar.Length; i++)
+            {
+                cikisZamani = molalar[i];
+                girisCikisFarki = DateTime.Parse(cikisZamani).Subtract(DateTime.Parse(girisZamani));
+                if (girisCikisFarki.TotalSeconds > 0)
+                {
+                    calismaSuresi = girisCikisFarki.ToString();
+                    index = i;
+                    break;
+                }
+                else
+                {
+                    cikisZamani = "23:59";
+                    girisCikisFarki = DateTime.Parse(cikisZamani).Subtract(DateTime.Parse(girisZamani));
+                    lblMolaBitis2.Text = (girisCikisFarki.Hours + 16).ToString() + " Saat " + (girisCikisFarki.Minutes + 30) + " Dakika " + girisCikisFarki.Seconds.ToString() + " Saniye";
+                    goto giris;
+                }
+            }
+            lblMolaBitis2.Text = girisCikisFarki.Hours.ToString() + " Saat " + girisCikisFarki.Minutes.ToString() + " Dakika " + girisCikisFarki.Seconds.ToString() + " Saniye";
+        giris:
+            LabelsControl();
+            
+            if (girisCikisFarki.TotalSeconds == 1)
+            {
+                if (index == molalar.Length - 1)
+                {
+                    timerMola.Stop();
+                    lblMolaBitis2.Text = "00:00:00";
+                    MessageBox.Show("Dersiniz Bitmiştir. İyi Akşamlar..");
+                }
+                else
+                {
+                    MessageBox.Show("Molaya Çıkabilirsiniz!!! Mola Saati : " + molalar[index]);
+                    timerMola.Stop();
+                    timerMola.Start();
+                }
+
+            }
+        }
+
+        private void timerGün_Tick(object sender, EventArgs e)
+        {
+            string girisZamani = DateTime.Now.ToLongTimeString();
+            string cikisZamani = "23:59:59";
+            TimeSpan girisCikisFarki = girisCikisFarki = DateTime.Parse(cikisZamani).Subtract(DateTime.Parse(girisZamani));
+            lblGunBitis2.Text = girisCikisFarki.Hours.ToString() + " Saat " + girisCikisFarki.Minutes.ToString() + " Dakika " + girisCikisFarki.Seconds.ToString() + " Saniye";
+        }
+
+        private void timerHafta_Tick(object sender, EventArgs e)
+        {
+            string day = DateTime.Now.DayOfWeek.ToString();
+            int gun = 0;
+
+            switch (day)
+            {
+                case "Monday":
+                    gun = 1;
+                    break;
+                case "Tuesday":
+                    gun = 2;
+                    break;
+                case "Wednesday":
+                    gun = 3;
+                    break;
+                case "Thursday":
+                    gun = 4;
+                    break;
+                case "Friday":
+                    gun = 5;
+                    break;
+                case "Saturday":
+                    gun = 6;
+                    break;
+                case "Sunday":
+                    gun = 7;
+                    break;
+            }
+
+            string girisZamani = DateTime.Now.ToLongTimeString();
+            string cikisZamani = "23:59:59";
+            int gunFarki = 7 - gun;
+            TimeSpan girisCikisFarki = girisCikisFarki = DateTime.Parse(cikisZamani).Subtract(DateTime.Parse(girisZamani));
+            lblHaftaBitis2.Text = gunFarki + " Gün " + girisCikisFarki.Hours.ToString() + " Saat " + girisCikisFarki.Minutes.ToString() + " Dakika " + girisCikisFarki.Seconds.ToString() + " Saniye";
+
+        }
+
+        private void timerEgitim_Tick(object sender, EventArgs e)
+        {
+            string[] sub = new string[4];
+            TimeSpan bitis = DateTime.Parse("30.03.2022 20:00:00").Subtract(DateTime.Now);
+
+            lblEgitimBitis2.Text = bitis.Days.ToString() + " Gün " + bitis.Hours.ToString() + " Saat " + bitis.Minutes.ToString() + " Dakika " + bitis.Seconds.ToString() + " Saniye";
+        }
+    }
+}
